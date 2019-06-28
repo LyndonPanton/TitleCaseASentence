a7687f7f5fcfb506748e8575e5bd5641f62daf80
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-28 13:26:09 +0100

    Added logic for title casing a sentence

--- js/app.js
+++ js/app.js
@@ -3,8 +3,26 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function titleCase(string) {
+		// Method I
+		// let arr = string.split(" ");
+		// arr.forEach(function(value, index) {
+		// 	arr[index] = arr[index][0].toUpperCase() + arr[index].slice(1).toLowerCase();
+		// });
+
+		// return arr.join(" ");
+
+
+		// Method II
+		return string.split(" ").map(function(value, index) {
+			return value[0].toUpperCase() + value.slice(1).toLowerCase();
+		}).join(" ");
+	}
+
 	let form = document.getElementById("form");
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
+
+		titleCase(this.children[0].value);
 	});
 };
