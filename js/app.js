f47eb53ad9310d83d3de35aba295f9d48f741272
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-28 14:19:11 +0100

    Added logic accounting for empty input

--- js/app.js
+++ js/app.js
@@ -4,23 +4,32 @@ window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function display(string) {
+		document.getElementById("display").textContent = "???";
 		document.getElementById("display").textContent = string;
 	}
 
 	function titleCase(string) {
 		// Method I
-		// let arr = string.split(" ");
-		// arr.forEach(function(value, index) {
-		// 	arr[index] = arr[index][0].toUpperCase() + arr[index].slice(1).toLowerCase();
-		// });
+		// if (string === "") {
+		// 	display("???");
+		// } else {
+		// 	let arr = string.split(" ");
+		// 	arr.forEach(function(value, index) {
+		// 		arr[index] = arr[index][0].toUpperCase() + arr[index].slice(1).toLowerCase();
+		// 	});
 
-		// return arr.join(" ");
+		// 	return display(arr.join(" "));
+		// }
 
 
 		// Method II
-		return display(string.split(" ").map(function(value, index) {
-					return value[0].toUpperCase() + value.slice(1).toLowerCase();
-				}).join(" "));
+		if (string === "") {
+			display("???");
+		} else {
+			display(string.split(" ").map(function(value, index) {
+						return value[0].toUpperCase() + value.slice(1).toLowerCase();
+					}).join(" "));
+		}
 	}
 
 	let form = document.getElementById("form");
